260da420c09e9b2351bbe9df8e044492e7738ca7
devgenjin77 <devgenjin77@gmail.com>
2022-01-16 13:11:01 +0900

    Add Solution ABC153_B

---
+++ app/src/main/java/contests/abc/abc153/abc153_b/Main.java
@@ -0,0 +1,32 @@
+/*
+ * ABC153
+ * B - Common Raccoon vs Monster
+ * https://atcoder.jp/contests/abc153/tasks/abc153_a
+ *
+ * verified:
+ * - https://atcoder.jp/contests/abc153/submissions/28577276
+ *
+ */
+package contests.abc.abc153.abc153_b;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in, StandardCharsets.UTF_8));
+    StringTokenizer head = new StringTokenizer(br.readLine());
+    int h = Integer.parseInt(head.nextToken());
+    int n = Integer.parseInt(head.nextToken());
+    int sum_a = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).sum();
+    br.close();
+    System.out.println(sum_a >= h ? "Yes" : "No");
+    return;
+  }
+}
