9fb9d98c938ee00ad2b68a1eb54a216a84ead78e
devgenjin77 <devgenjin77@gmail.com>
2022-01-20 01:27:04 +0900

    Fix Solution ABC154_E

--- app/src/main/java/contests/abc/abc154/abc154_e/Main.java
+++ app/src/main/java/contests/abc/abc154/abc154_e/Main.java
@@ -4,7 +4,7 @@
  * https://atcoder.jp/contests/abc154/tasks/abc154_e
  *
  * verified:
- * - https://atcoder.jp/contests/abc154/submissions/28639058
+ * - https://atcoder.jp/contests/abc154/submissions/28639626
  *
  */
 package contests.abc.abc154.abc154_e;
@@ -20,26 +20,26 @@ public class Main {
     String n = br.readLine();
     int k = Integer.parseInt(br.readLine());
     br.close();
-    long[] pow9 = new long[k + 1];
+    int[] pow9 = new int[k + 1];
     pow9[0] = 1;
     for (int a = 1; a <= k; a++) {
       pow9[a] = pow9[a - 1] * 9;
     }
     // 最大値Nに含まれる0以外の桁の数
-    int ne0 = 0;
-    int nlen = n.length();
-    long ans = 0;
-    for (int i = 0; i < nlen; i++) {
+    int cntNot0 = 0;
+    int len = n.length();
+    int ans = 0;
+    for (int i = 0; i < len; i++) {
       int d = n.charAt(i) - '0';
       if (d == 0) {
         continue;
       }
       //i桁目を0設定した場合の数
-      ans += nCr(nlen - i - 1, k - ne0) * pow9[k - ne0];
-      //i桁目を0以外を設定した場合の数
-      ans += (d - 1) * nCr(nlen - i - 1, k - ne0 - 1) * pow9[k - ne0 - 1];
-      ne0 += 1;
-      if (ne0 == k) {
+      ans += nCr(len - i - 1, k - cntNot0) * pow9[k - cntNot0];
+      //i桁目を1以上d未満で設定した場合の数
+      ans += (d - 1) * nCr(len - i - 1, k - cntNot0 - 1) * pow9[k - cntNot0 - 1];
+      cntNot0 += 1;
+      if (cntNot0 == k) {
         ans += d;
         break;
       }
