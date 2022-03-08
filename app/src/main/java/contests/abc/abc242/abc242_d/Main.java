c2766eda3429d1ef912884337c5917e9fb0f4d9f
devgenjin77 <devgenjin77@gmail.com>
2022-03-09 00:09:00 +0900

    Fix Solution ABC242_D

--- app/src/main/java/contests/abc/abc242/abc242_d/Main.java
+++ app/src/main/java/contests/abc/abc242/abc242_d/Main.java
@@ -4,53 +4,152 @@
  * https://atcoder.jp/contests/abc242/tasks/abc242_d
  *
  * verified:
- * - https://atcoder.jp/contests/abc242/submissions/29917677
+ * - https://atcoder.jp/contests/abc242/submissions/29960654
  *
  */
 package contests.abc.abc242.abc242_d;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
 
 public class Main {
 
-  static final String ABC = "ABC";
-
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String s = br.readLine();
-    int q = Integer.parseInt(br.readLine());
-    long[][] query = new long[q][2];
-    for (int i = 0; i < q; i++) {
-      String[] input = br.readLine().split(" ");
-      query[i][0] = Long.parseLong(input[0]);
-      query[i][1] = Long.parseLong(input[1]);
-    }
-    br.close();
-
+    FastScanner sc = new FastScanner();
+    String s = sc.next();
+    int q = sc.nextInt();
     PrintWriter pw = new PrintWriter(System.out);
     for (int i = 0; i < q; i++) {
-      pw.println(ABC.charAt(func(s, query[i][0], query[i][1] - 1)));
+      long t = sc.nextLong();
+      long k = sc.nextLong();
+      pw.println(solve(s, t, k - 1));
     }
     pw.close();
+    sc.close();
   }
 
-  static int func(String s, long t, long k) {
-    int ret = 0;
-    if (t == 0) {
-      ret = s.charAt((int) k) - 'A';
-    } else if (k == 0) {
-      ret = s.charAt(0) - 'A';
-      ret += (int) (t % 3);
-    } else {
-      if (k % 2 == 0) {
-        ret = func(s, t - 1, k / 2) + 1;
+  static char solve(String s, long t, long k) {
+    long tmp_k = k;
+    long tmp_t = t;
+    long offset = 0;
+    while (tmp_t > 0 && tmp_k > 0) {
+      if ((tmp_k & 1l) == 1) {
+        offset += 2;
       } else {
-        ret = func(s, t - 1, k / 2) + 2;
+        offset += 1;
+      }
+      tmp_k >>= 1;
+      tmp_t--;
+    }
+    int d = (int) (offset) % 3;
+    if (tmp_t > 0) {
+      d += (tmp_t % 3) + (s.charAt(0) - 'A');
+    } else {
+      d += s.charAt((int) tmp_k) - 'A';
+    }
+    return "ABC".charAt(d % 3);
+  }
+
+  // ライブラリ
+  static class FastScanner {
+
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    private boolean hasNextByte() {
+      if (ptr < buflen) {
+        return true;
+      } else {
+        ptr = 0;
+        try {
+          buflen = in.read(buffer);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        if (buflen <= 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private int readByte() {
+      if (hasNextByte()) {
+        return buffer[ptr++];
+      } else {
+        return -1;
       }
     }
-    return ret % 3;
+
+    private static boolean isPrintableChar(int c) {
+      return 33 <= c && c <= 126;
+    }
+
+    public boolean hasNext() {
+      while (hasNextByte() && !isPrintableChar(buffer[ptr])) {
+        ptr++;
+      }
+      return hasNextByte();
+    }
+
+    public String next() {
+      if (!hasNext()) {
+        throw new NoSuchElementException();
+      }
+      StringBuilder sb = new StringBuilder();
+      int b = readByte();
+      while (isPrintableChar(b)) {
+        sb.appendCodePoint(b);
+        b = readByte();
+      }
+      return sb.toString();
+    }
+
+    public long nextLong() {
+      if (!hasNext()) {
+        throw new NoSuchElementException();
+      }
+      long n = 0;
+      boolean minus = false;
+      int b = readByte();
+      if (b == '-') {
+        minus = true;
+        b = readByte();
+      }
+      if (b < '0' || '9' < b) {
+        throw new NumberFormatException();
+      }
+      while (true) {
+        if ('0' <= b && b <= '9') {
+          n *= 10;
+          n += b - '0';
+        } else if (b == -1 || !isPrintableChar(b)) {
+          return minus ? -n : n;
+        } else {
+          throw new NumberFormatException();
+        }
+        b = readByte();
+      }
+    }
+
+    public int nextInt() {
+      long nl = nextLong();
+      if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) {
+        throw new NumberFormatException();
+      }
+      return (int) nl;
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public void close() throws IOException {
+      in.close();
+    }
   }
 }
