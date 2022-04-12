936e92a35d5926820c84a79fae1cada0b1807bd4
devgenjin77 <devgenjin77@gmail.com>
2022-04-12 22:25:41 +0900

    Modify Library FenwickTree

--- app/src/main/java/library/util/FenwickTree.java
+++ app/src/main/java/library/util/FenwickTree.java
@@ -5,49 +5,51 @@
  */
 package library.util;
 
-class FenwickTree {
+//FenwickTreeライブラリ
+public class FenwickTree {
+
   final int N;
   long[] data;
 
-  FenwickTree(int n){
+  public FenwickTree(int n) {
     N = n;
     this.data = new long[n];
   }
 
-  FenwickTree(long[] data){
+  public FenwickTree(long[] data) {
     N = data.length;
     this.data = new long[this.N];
     System.arraycopy(data, 0, this.data, 0, N);
-    for(int i = 1; i <= N; i++){
+    for (int i = 1; i <= N; i++) {
       int p = i + (-i & i);
-      if(p <= N) {
+      if (p <= N) {
         this.data[p - 1] += this.data[i - 1];
       }
     }
   }
 
-  void add(int p, long x){
-    if(!(p >= 0 && p < N)){
+  public void add(int p, long x) {
+    if (!(p >= 0 && p < N)) {
       String errMsg = String.format("Index %d out of bounds for length %d.", p, N);
       throw new ArrayIndexOutOfBoundsException(errMsg);
     }
     p++;
-    while(p <= N){
+    while (p <= N) {
       data[p - 1] += x;
       p += -p & p;
     }
   }
 
-  long sum(int l, int r){
-    if(l > r){
+  public long sum(int l, int r) {
+    if (l > r) {
       String errMsg = String.format("Invalid range: [%d, %d).", l, r);
       throw new IllegalArgumentException(errMsg);
     }
-    if(!(l >= 0 && l <= N)){
+    if (!(l >= 0 && l <= N)) {
       String errMsg = String.format("Index %d out of bounds for length %d.", l, N);
       throw new ArrayIndexOutOfBoundsException(errMsg);
     }
-    if(!(r >= 0 && r <= N)){
+    if (!(r >= 0 && r <= N)) {
       String errMsg = String.format("Index %d out of bounds for length %d.", r, N);
       throw new ArrayIndexOutOfBoundsException(errMsg);
     }
@@ -56,7 +58,7 @@ class FenwickTree {
 
   private long sum(int r) {
     long s = 0;
-    while(r > 0){
+    while (r > 0) {
       s += data[r - 1];
       r -= -r & r;
     }
