86da109322f187ec3d6017ad50ce98586762f221
devgenjin77 <devgenjin77@gmail.com>
2022-04-29 20:20:45 +0900

    Fix Solution PRACTICE2_A

--- app/src/main/java/contests/practice2/practice2_a/Main.java
+++ app/src/main/java/contests/practice2/practice2_a/Main.java
@@ -4,7 +4,7 @@
  * https://atcoder.jp/contests/practice2/tasks/practice2_a
  *
  * verified:
- * - https://atcoder.jp/contests/practice2/submissions/31336829
+ * - https://atcoder.jp/contests/practice2/submissions/31343353
  *
  */
 
@@ -33,130 +33,122 @@ public class Main {
     pw.close();
     sc.close();
   }
+}
 
-  //DisjointSetUnionライブラリ
-  static class DisjointSetUnion {
+//DisjointSetUnionライブラリ
+class DisjointSetUnion {
 
-    private final int n;
-    private int[] parent_or_size;
+  private final int n;
+  private final int[] parent_or_size;
 
-    public DisjointSetUnion(int n) {
-      this.n = n;
-      parent_or_size = new int[n];
-      java.util.Arrays.fill(parent_or_size, -1);
+  public DisjointSetUnion(int n) {
+    this.n = n;
+    parent_or_size = new int[n];
+    java.util.Arrays.fill(parent_or_size, -1);
+  }
+
+  public int merge(int a, int b) {
+    if (!(0 <= a && a < n) || !(0 <= b && b < n)) {
+      return -1;
     }
-
-    public int merge(int a, int b) {
-      if (!(0 <= a && a < n) || !(0 <= b && b < n)) {
-        return -1;
-      }
-      int x = leader(a);
-      int y = leader(b);
-      if (x == y) {
-        return x;
-      }
-      if (-parent_or_size[x] < -parent_or_size[y]) {
-        int temp = x;
-        x = y;
-        y = temp;
-      }
-      parent_or_size[x] += parent_or_size[y];
-      parent_or_size[y] = x;
+    int x = leader(a);
+    int y = leader(b);
+    if (x == y) {
       return x;
     }
-
-    public boolean isSame(int a, int b) {
-      if (!(0 <= a && a < n) || !(0 <= b && b < n)) {
-        return false;
-      }
-      return leader(a) == leader(b);
+    if (-parent_or_size[x] < -parent_or_size[y]) {
+      int temp = x;
+      x = y;
+      y = temp;
     }
+    parent_or_size[x] += parent_or_size[y];
+    parent_or_size[y] = x;
+    return x;
+  }
 
-    public int leader(int a) {
-      if (parent_or_size[a] < 0) {
-        return a;
-      } else {
-        parent_or_size[a] = leader(parent_or_size[a]);
-        return parent_or_size[a];
-      }
+  public boolean isSame(int a, int b) {
+    if (!(0 <= a && a < n) || !(0 <= b && b < n)) {
+      return false;
     }
+    return leader(a) == leader(b);
+  }
 
-    public int size(int a) {
-      if (!(0 <= a && a < n)) {
-        return -1;
-      }
-      return -parent_or_size[leader(a)];
-    }
-
-    public java.util.List<java.util.List<Integer>> groups() {
-      int[] leader_buf = new int[n];
-      int[] group_size = new int[n];
-      for (int i = 0; i < n; i++) {
-        leader_buf[i] = leader(i);
-        group_size[leader_buf[i]]++;
-      }
-      java.util.List<java.util.List<Integer>> result = new java.util.ArrayList<>();
-      java.util.HashMap<Integer, java.util.ArrayList<Integer>> list_map = new java.util.HashMap<>();
-      for (int i = 0; i < n; i++) {
-        if (i == leader_buf[i]) {
-          list_map.put(i, new java.util.ArrayList<>());
-        }
-      }
-      for (int i = 0; i < n; i++) {
-        list_map.get(leader_buf[i]).add(i);
-      }
-      for (Integer key : list_map.keySet()) {
-        result.add(list_map.get(key));
-      }
-      return result;
+  public int leader(int a) {
+    if (parent_or_size[a] < 0) {
+      return a;
+    } else {
+      parent_or_size[a] = leader(parent_or_size[a]);
+      return parent_or_size[a];
     }
   }
 
-  //NextScannerライブラリ
-  static class NextScanner implements AutoCloseable {
-
-    final private java.io.InputStreamReader reader;
-
-    private java.util.StringTokenizer st;
-
-    private static final int BUF_SIZE = 1024;
-
-    private static final char[] buf = new char[BUF_SIZE];
-
-    public NextScanner(java.io.InputStream is) {
-      this.reader = new java.io.InputStreamReader(is);
-      init();
+  public int size(int a) {
+    if (!(0 <= a && a < n)) {
+      return -1;
     }
+    return -parent_or_size[leader(a)];
+  }
 
-    private void init() {
-      StringBuilder sb = new StringBuilder();
-      int len = 0;
-      while (true) {
-        try {
-          int r = this.reader.read(buf, 0, BUF_SIZE);
-          if (r < 0) {
-            break;
-          } else {
-            len += r;
-            sb.append(buf);
-          }
-        } catch (java.io.IOException ioe) {
-          throw new java.util.InputMismatchException();
+  public java.util.List<java.util.List<Integer>> groups() {
+    int m = 0;
+    java.util.List<java.util.List<Integer>> result = new java.util.ArrayList<>();
+    java.util.Map<Integer, Integer> map_leader = new java.util.HashMap<>();
+    for (int i = 0; i < n; i++) {
+      int lead = leader(i);
+      if (!map_leader.containsKey(lead)) {
+        map_leader.put(lead, m++);
+        result.add(new java.util.ArrayList<>());
+      }
+      result.get(map_leader.get(lead)).add(i);
+    }
+    return result;
+  }
+}
+
+//NextScannerライブラリ
+class NextScanner implements AutoCloseable {
+
+  final private java.io.InputStreamReader reader;
+
+  private java.util.StringTokenizer st;
+
+  private static final int BUF_SIZE = 1024;
+
+  private static final char[] buf = new char[BUF_SIZE];
+
+  public NextScanner(java.io.InputStream is) {
+    this.reader = new java.io.InputStreamReader(is);
+    init();
+  }
+
+  private void init() {
+    StringBuilder sb = new StringBuilder();
+    int len = 0;
+    while (true) {
+      try {
+        int r = this.reader.read(buf, 0, BUF_SIZE);
+        if (r < 0) {
+          break;
+        } else {
+          len += r;
+          sb.append(buf);
         }
+      } catch (java.io.IOException ioe) {
+        throw new java.util.InputMismatchException();
       }
-      st = new java.util.StringTokenizer(sb.substring(0, len));
     }
+    st = new java.util.StringTokenizer(sb.substring(0, len));
+  }
 
-    public String next() {
-      if (st == null || !st.hasMoreElements()) {
-        throw new java.util.NoSuchElementException();
-      }
-      return st.nextToken();
+  public String next() {
+    if (st == null || !st.hasMoreElements()) {
+      throw new java.util.NoSuchElementException();
     }
+    return st.nextToken();
+  }
 
-    @Override
-    public void close() throws Exception {
-      reader.close();
-    }
+  @Override
+  public void close() throws Exception {
+    reader.close();
   }
 }
