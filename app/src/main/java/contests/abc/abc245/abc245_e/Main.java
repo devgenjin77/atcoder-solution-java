17b99e2cf022227a5329703710ab6ac009102263
devgenjin77 <devgenjin77@gmail.com>
2022-05-03 01:31:28 +0900

    Fix Solution ABC245_E

--- app/src/main/java/contests/abc/abc245/abc245_e/Main.java
+++ app/src/main/java/contests/abc/abc245/abc245_e/Main.java
@@ -4,7 +4,7 @@
  * https://atcoder.jp/contests/abc245/tasks/abc245_e
  *
  * verified:
- * - https://atcoder.jp/contests/abc245/submissions/30535223
+ * - https://atcoder.jp/contests/abc245/submissions/31404596
  *
  */
 
@@ -18,182 +18,132 @@ import java.util.TreeMap;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    FastScanner fs = new FastScanner();
-    int n = fs.nextInt();
-    int m = fs.nextInt();
+    NextScanner sc = new NextScanner(System.in);
+    int n = Integer.parseInt(sc.next());
+    int m = Integer.parseInt(sc.next());
     int[] array_a = new int[n];
     int[] array_b = new int[n];
     int[] array_c = new int[m];
     int[] array_d = new int[m];
     for (int i = 0; i < n; i++) {
-      array_a[i] = fs.nextInt();
+      array_a[i] = Integer.parseInt(sc.next());
     }
     for (int i = 0; i < n; i++) {
-      array_b[i] = fs.nextInt();
+      array_b[i] = Integer.parseInt(sc.next());
     }
     for (int i = 0; i < m; i++) {
-      array_c[i] = fs.nextInt();
+      array_c[i] = Integer.parseInt(sc.next());
     }
     for (int i = 0; i < m; i++) {
-      array_d[i] = fs.nextInt();
+      array_d[i] = Integer.parseInt(sc.next());
     }
-    fs.close();
-
-    List<IntPair> list_choco = new ArrayList<>();
-    List<IntPair> list_box = new ArrayList<>();
+    sc.close();
+    List<RectangleShape> list_shape = new ArrayList<>();
     for (int i = 0; i < n; i++) {
-      list_choco.add(new IntPair(array_a[i], array_b[i]));
+      list_shape.add(new RectangleShape(RectangleShape.TYPE_CHOCO, array_a[i], array_b[i]));
     }
     for (int i = 0; i < m; i++) {
-      list_box.add(new IntPair(array_c[i], array_d[i]));
+      list_shape.add(new RectangleShape(RectangleShape.TYPE_BOX, array_c[i], array_d[i]));
     }
-    Collections.sort(list_choco);
-    Collections.sort(list_box);
-    boolean isOk = true;
-    TreeMap<Integer, Integer> stock_box_len = new TreeMap<>();
-    int idx_choco = n - 1;
-    int idx_box = m - 1;
-    while (idx_choco >= 0) {
-      int width_choco = list_choco.get(idx_choco).first;
-      while (idx_box >= 0 && list_box.get(idx_box).first >= width_choco) {
-        int len_box = list_box.get(idx_box).second;
-        stock_box_len.put(len_box, stock_box_len.getOrDefault(len_box, 0) + 1);
-        idx_box--;
-      }
-      Integer k = stock_box_len.ceilingKey(list_choco.get(idx_choco).second);
-      if (k == null) {
-        isOk = false;
-        break;
+    Collections.sort(list_shape);
+    TreeMap<Integer, Integer> map_length_cnt = new TreeMap<>();
+    boolean result = true;
+    for (int idx = list_shape.size() - 1; idx >= 0; idx--) {
+      RectangleShape rs = list_shape.get(idx);
+      if (rs.type == RectangleShape.TYPE_BOX) {
+        map_length_cnt.put(rs.length, map_length_cnt.getOrDefault(rs.length, 0) + 1);
       } else {
-        int stock = stock_box_len.get(k);
-        if (stock > 1) {
-          stock_box_len.put(k, stock - 1);
+        Integer ceil_key = map_length_cnt.ceilingKey(rs.length);
+        if (ceil_key == null) {
+          result = false;
+          break;
         } else {
-          stock_box_len.remove(k);
+          int stock_len = map_length_cnt.get(ceil_key) - 1;
+          if (stock_len > 0) {
+            map_length_cnt.put(ceil_key, stock_len);
+          } else {
+            map_length_cnt.remove(ceil_key);
+          }
         }
       }
-      idx_choco--;
     }
-    System.out.println(isOk ? "Yes" : "No");
+    System.out.println(result ? "Yes" : "No");
+  }
+}
+
+class RectangleShape implements Comparable<RectangleShape> {
+
+  static final int TYPE_CHOCO = 1;
+
+  static final int TYPE_BOX = 2;
+
+  final int type, width, length;
+
+  RectangleShape(int type, int width, int length) {
+    this.type = type;
+    this.width = width;
+    this.length = length;
   }
 
-  static class IntPair implements Comparable<IntPair> {
-
-    int first, second;
-
-    IntPair(int first, int second) {
-      this.first = first;
-      this.second = second;
-    }
-
-    @Override
-    public int compareTo(IntPair o) {
-      if (first != o.first) {
-        return Integer.compare(first, o.first);
+  @Override
+  public int compareTo(RectangleShape o) {
+    int cmp = Integer.compare(width, o.width);
+    if (cmp == 0) {
+      if(type == o.type) {
+        return Integer.compare(length, o.length);
+      } else {
+        return type == TYPE_BOX ? 1 : -1;
       }
-      return Integer.compare(second, o.second);
+    } else {
+      return cmp;
     }
   }
+}
 
-  //FastScanner ライブラリ
-  static class FastScanner implements AutoCloseable {
+//NextScannerライブラリ
+class NextScanner implements AutoCloseable {
 
-    private final java.io.InputStream in = System.in;
-    private final byte[] buffer = new byte[1024];
-    private int ptr = 0;
-    private int buf_len = 0;
+  private final java.io.BufferedReader br;
 
-    private boolean hasNextByte() {
-      if (ptr < buf_len) {
-        return true;
-      } else {
-        ptr = 0;
-        try {
-          buf_len = in.read(buffer);
-        } catch (java.io.IOException e) {
-          e.printStackTrace();
-        }
-        if (buf_len <= 0) {
-          return false;
-        }
-      }
-      return true;
-    }
+  private java.util.StringTokenizer st;
 
-    private int readByte() {
-      if (hasNextByte()) {
-        return buffer[ptr++];
-      } else {
-        return -1;
-      }
-    }
+  private static final int BUF_SIZE = 1024;
 
-    private static boolean isPrintableChar(int c) {
-      return 33 <= c && c <= 126;
-    }
+  private static final char[] c_buf = new char[BUF_SIZE];
 
-    public boolean hasNext() {
-      while (hasNextByte() && !isPrintableChar(buffer[ptr])) {
-        ptr++;
-      }
-      return hasNextByte();
-    }
+  public NextScanner(java.io.InputStream input) {
+    this.br = new java.io.BufferedReader(new java.io.InputStreamReader(input));
+  }
 
-    public String next() {
-      if (!hasNext()) {
+  private java.util.StringTokenizer readInput() {
+    java.util.StringTokenizer st;
+    try {
+      int b = br.read(c_buf);
+      if (b == BUF_SIZE) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(c_buf);
+        sb.append(br.readLine());
+        st = new java.util.StringTokenizer(sb.toString());
+      } else if (b < 0) {
         throw new java.util.NoSuchElementException();
+      } else {
+        st = new java.util.StringTokenizer(new String(c_buf, 0, b));
       }
-      StringBuilder sb = new StringBuilder();
-      int b = readByte();
-      while (isPrintableChar(b)) {
-        sb.appendCodePoint(b);
-        b = readByte();
-      }
-      return sb.toString();
+    } catch (java.io.IOException e) {
+      throw new java.util.InputMismatchException(e.getMessage());
     }
+    return st;
+  }
 
-    public long nextLong() {
-      if (!hasNext()) {
-        throw new java.util.NoSuchElementException();
-      }
-      long n = 0;
-      boolean minus = false;
-      int b = readByte();
-      if (b == '-') {
-        minus = true;
-        b = readByte();
-      }
-      if (b < '0' || '9' < b) {
-        throw new NumberFormatException();
-      }
-      while (true) {
-        if ('0' <= b && b <= '9') {
-          n *= 10;
-          n += b - '0';
-        } else if (b == -1 || !isPrintableChar(b)) {
-          return minus ? -n : n;
-        } else {
-          throw new NumberFormatException();
-        }
-        b = readByte();
-      }
+  public String next() throws java.io.IOException {
+    if (st == null || !st.hasMoreElements()) {
+      st = readInput();
     }
+    return st.nextToken();
+  }
 
-    public int nextInt() {
-      long nl = nextLong();
-      if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) {
-        throw new NumberFormatException();
-      }
-      return (int) nl;
-    }
-
-    public double nextDouble() {
-      return Double.parseDouble(next());
-    }
-
-    @Override
-    public void close() throws Exception {
-      in.close();
-    }
+  @Override
+  public void close() throws Exception {
+    br.close();
   }
 }
