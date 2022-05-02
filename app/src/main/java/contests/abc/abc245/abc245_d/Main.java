f4013cf4c047bfd1b2d620fd909e129821137c82
devgenjin77 <devgenjin77@gmail.com>
2022-05-03 00:19:39 +0900

    Fix Solution ABC245_D

--- app/src/main/java/contests/abc/abc245/abc245_d/Main.java
+++ app/src/main/java/contests/abc/abc245/abc245_d/Main.java
@@ -4,46 +4,88 @@
  * https://atcoder.jp/contests/abc245/tasks/abc245_d
  *
  * verified:
- * - https://atcoder.jp/contests/abc245/submissions/30508654
+ * - https://atcoder.jp/contests/abc245/submissions/31403854
  *
  */
 
 package contests.abc.abc245.abc245_d;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
-    int n = Integer.parseInt(input[0]);
-    int m = Integer.parseInt(input[1]);
-    String[] input_a = br.readLine().split(" ");
-    String[] input_c = br.readLine().split(" ");
-    br.close();
-    int[] array_a = new int[n + 1];
+  public static void main(String[] args) throws Exception {
+    NextScanner sc = new NextScanner(System.in);
+    int n = Integer.parseInt(sc.next());
+    int m = Integer.parseInt(sc.next());
+    int[] arr_a = new int[n + 1];
+    int[] arr_c = new int[n + m + 1];
     for (int i = 0; i <= n; i++) {
-      array_a[i] = Integer.parseInt(input_a[i]);
+      arr_a[i] = Integer.parseInt(sc.next());
     }
-    int[] array_c = new int[n + m + 1];
     for (int i = 0; i <= n + m; i++) {
-      array_c[i] = Integer.parseInt(input_c[i]);
+      arr_c[i] = Integer.parseInt(sc.next());
     }
-    int[] array_b = new int[m + 1];
-    for(int idx_b = m; idx_b >= 0; idx_b--) {
-      array_b[idx_b] = array_c[n + idx_b] / array_a[n];
-      for(int idx_a = 0; idx_a <= n; idx_a++) {
-        array_c[idx_a + idx_b] -= array_a[idx_a] * array_b[idx_b];
+    sc.close();
+
+    int[] arr_b = new int[m + 1];
+    for (int idx_b = m; idx_b >= 0; idx_b--) {
+      arr_b[idx_b] = arr_c[idx_b + n] / arr_a[n];
+      for (int idx_a = 0; idx_a < n; idx_a++) {
+        arr_c[idx_a + idx_b] -= arr_a[idx_a] * arr_b[idx_b];
       }
     }
     StringBuilder sb = new StringBuilder();
-    sb.append(array_b[0]);
-    for (int i = 1; i <= m; i++) {
-      sb.append(" " + array_b[i]);
+    sb.append(arr_b[0]);
+    for (int i = 1; i < arr_b.length; i++) {
+      sb.append(" ").append(arr_b[i]);
     }
     System.out.println(sb.toString());
+  }
+}
+
+//NextScannerライブラリ
+class NextScanner implements AutoCloseable {
+
+  private final java.io.BufferedReader br;
+
+  private java.util.StringTokenizer st;
+
+  private static final int BUF_SIZE = 1024;
+
+  private static final char[] c_buf = new char[BUF_SIZE];
+
+  public NextScanner(java.io.InputStream input) {
+    this.br = new java.io.BufferedReader(new java.io.InputStreamReader(input));
+  }
+
+  private java.util.StringTokenizer readInput() {
+    java.util.StringTokenizer st;
+    try {
+      int b = br.read(c_buf);
+      if (b == BUF_SIZE) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(c_buf);
+        sb.append(br.readLine());
+        st = new java.util.StringTokenizer(sb.toString());
+      } else if (b < 0) {
+        throw new java.util.NoSuchElementException();
+      } else {
+        st = new java.util.StringTokenizer(new String(c_buf, 0, b));
+      }
+    } catch (java.io.IOException e) {
+      throw new java.util.InputMismatchException(e.getMessage());
+    }
+    return st;
+  }
+
+  public String next() throws java.io.IOException {
+    if (st == null || !st.hasMoreElements()) {
+      st = readInput();
+    }
+    return st.nextToken();
+  }
+
+  @Override
+  public void close() throws Exception {
+    br.close();
   }
 }
