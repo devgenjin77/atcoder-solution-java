f2b6a2c2eb40136324ee4702c65517dcda05f38a
devgenjin77 <devgenjin77@gmail.com>
2022-05-18 01:19:00 +0900

    Resolve Problem ABC213_A

--- app/src/main/java/contests/abc/abc21x/abc213/abc213_a/Main.java
+++ app/src/main/java/contests/abc/abc21x/abc213/abc213_a/Main.java
@@ -4,22 +4,24 @@
  * https://atcoder.jp/contests/abc213/tasks/abc213_a
  *
  * verified:
- * - https://atcoder.jp/contests/abc213/submissions/24898702
+ * - https://atcoder.jp/contests/abc213/submissions/31770468
  */
-package contests.abc.abc213.abc213_a;
+
+package contests.abc.abc21x.abc213.abc213_a;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
     br.close();
-    int a = Integer.parseInt(input[0]);
-    int b = Integer.parseInt(input[1]);
     System.out.println(a ^ b);
   }
 }
