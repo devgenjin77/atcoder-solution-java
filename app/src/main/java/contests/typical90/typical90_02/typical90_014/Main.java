0d45639a14cbd0334948b09523c96a98a0bd0f6b
devgenjin77 <devgenjin77@gmail.com>
2022-05-22 20:26:24 +0900

    Resolve Solution TYPICAL90_014

--- app/src/main/java/contests/typical90/typical90_02/typical90_014/Main.java
+++ app/src/main/java/contests/typical90/typical90_02/typical90_014/Main.java
@@ -4,25 +4,35 @@
  * https://atcoder.jp/contests/typical90/tasks/typical90_n
  *
  * verified:
- * - https://atcoder.jp/contests/typical90/submissions/27941932
+ * - https://atcoder.jp/contests/typical90/submissions/31898985
+ *
+ * note:
+ * ソートして比較する
  *
  */
-package contests.typical90.typical90_014;
+
+package contests.typical90.typical90_02.typical90_014;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    int[] array_a = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    int[] array_b = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    StringTokenizer st_a = new StringTokenizer(br.readLine());
+    StringTokenizer st_b = new StringTokenizer(br.readLine());
     br.close();
+    long[] array_a = new long[n];
+    long[] array_b = new long[n];
+    for (int i = 0; i < n; i++) {
+      array_a[i] = Long.parseLong(st_a.nextToken());
+      array_b[i] = Long.parseLong(st_b.nextToken());
+    }
     Arrays.sort(array_a);
     Arrays.sort(array_b);
     long ans = 0;
