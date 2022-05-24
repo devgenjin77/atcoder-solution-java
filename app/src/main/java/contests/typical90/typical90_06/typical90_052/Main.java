7a48aaa253480fc466bd50a36178a5e792d497d7
devgenjin77 <devgenjin77@gmail.com>
2022-05-24 20:34:27 +0900

    Resolve Solution TYPICAL90_052

--- app/src/main/java/contests/typical90/typical90_06/typical90_052/Main.java
+++ app/src/main/java/contests/typical90/typical90_06/typical90_052/Main.java
@@ -4,30 +4,38 @@
  * https://atcoder.jp/contests/typical90/tasks/typical90_az
  *
  * verified:
- * - https://atcoder.jp/contests/typical90/submissions/28364893
+ * - https://atcoder.jp/contests/typical90/submissions/31933816
+ *
+ * note:
+ * -因数分解をする
  *
  */
-package contests.typical90.typical90_052;
+
+package contests.typical90.typical90_06.typical90_052;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  final static long MOD = 1_000_000_007;
+  static final long MOD = 1_000_000_007l;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
     long ans = 1;
     for (int i = 0; i < n; i++) {
-      long sum_of_dice = Stream.of(br.readLine().split(" ")).mapToLong(Long::parseLong).sum();
-      ans = (ans * sum_of_dice) % MOD;
+      StringTokenizer st_a = new StringTokenizer(br.readLine());
+      long dice_sum = 0;
+      while (st_a.hasMoreElements()) {
+        dice_sum += Long.parseLong(st_a.nextToken());
+      }
+      ans *= dice_sum;
+      ans %= MOD;
     }
     br.close();
     System.out.println(ans);
-    return;
   }
 }
