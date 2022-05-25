cd6a51a75ce93fd26930deaa5ac42ac3bb5f87f1
devgenjin77 <devgenjin77@gmail.com>
2022-05-26 00:30:35 +0900

    Resolve Solution TYPICAL90_069

--- app/src/main/java/contests/typical90/typical90_07/typical90_069/Main.java
+++ app/src/main/java/contests/typical90/typical90_07/typical90_069/Main.java
@@ -4,15 +4,18 @@
  * https://atcoder.jp/contests/typical90/tasks/typical90_bq
  *
  * verified:
- * - https://atcoder.jp/contests/typical90/submissions/28467887
+ * - https://atcoder.jp/contests/typical90/submissions/31953414
+ *
+ * note:
+ * -繰り返し二乗法をつかう
  *
  */
-package contests.typical90.typical90_069;
+
+package contests.typical90.typical90_07.typical90_069;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,34 +23,32 @@ public class Main {
   static final long MOD = 1_000_000_007L;
 
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(
-        new InputStreamReader(System.in, StandardCharsets.UTF_8));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     long n = Long.parseLong(st.nextToken());
     long k = Long.parseLong(st.nextToken());
     br.close();
-    System.out.println(solve(n, k, MOD));
-  }
 
-  static long solve(long n, long k, long mod){
-    long ans = k % mod;
-    if(n > 1){
-      ans = ans * (k - 1) % mod;
-    }
-    if(n > 2) {
-      if(k > 2) {
-        ans = ans * pow(k - 2, n - 2, mod) % mod;
+    long ans;
+
+    if (n == 1) {
+      ans = k;
+    } else if (n == 2) {
+      ans = k * (k - 1) % MOD;
+    } else {
+      if (k > 2) {
+        ans = (k * (k - 1) % MOD) * pow(k - 2, n - 2, MOD) % MOD;
       } else {
         ans = 0;
       }
     }
-    return ans;
+    System.out.println(ans);
   }
 
   static long pow(long x, long n, long mod) {
     long answer = 1l;
     while (n > 0) {
-      if((n & 1) == 1){
+      if ((n & 1) == 1) {
         answer = answer * x % mod;
       }
       x = x * x % mod;
