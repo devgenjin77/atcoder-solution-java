da24dfb8f60ee5fc7e967ab4c589a714bf756d52
devgenjin77 <devgenjin77@gmail.com>
2022-06-06 21:03:47 +0900

    Resolve Solution ABC240_A

--- app/src/main/java/contests/abc/abc24x/abc240/abc240_a/Main.java
+++ app/src/main/java/contests/abc/abc24x/abc240/abc240_a/Main.java
@@ -4,28 +4,25 @@
  * https://atcoder.jp/contests/abc240/tasks/abc240_a
  *
  * verified:
- * - https://atcoder.jp/contests/abc240/submissions/29641760
+ * - https://atcoder.jp/contests/abc240/submissions/32288676
  *
  */
-package contests.abc.abc240.abc240_a;
+
+package contests.abc.abc24x.abc240.abc240_a;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] input = br.readLine().split(" ");
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
     br.close();
-    int a = Integer.parseInt(input[0]);
-    int b = Integer.parseInt(input[1]);
-    System.out.println(solve(a, b) ? "Yes" : "No");
-  }
-
-  static boolean solve(int a, int b) {
-    int tmp = b - a;
-    return tmp == 1 || tmp == 9;
+    System.out.println((b - a == 1 || b - a == 9) ? "Yes" : "No");
   }
 }
