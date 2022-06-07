b644075e824510103a07cbb65738296d3012ef17
devgenjin77 <devgenjin77@gmail.com>
2022-06-07 23:03:33 +0900

    Resolve Solution ABC241_A

--- app/src/main/java/contests/abc/abc24x/abc241/abc241_a/Main.java
+++ app/src/main/java/contests/abc/abc24x/abc241/abc241_a/Main.java
@@ -4,22 +4,27 @@
  * https://atcoder.jp/contests/abc241/tasks/abc241_a
  *
  * verified:
- * - https://atcoder.jp/contests/abc241/submissions/29732790
+ * - https://atcoder.jp/contests/abc241/submissions/32308041
  *
  */
-package contests.abc.abc241.abc241_a;
+
+package contests.abc.abc24x.abc241.abc241_a;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int[] a = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] array_a = new int[10];
+    for (int i = 0; i < 10; i++) {
+      array_a[i] = Integer.parseInt(st.nextToken());
+    }
     br.close();
-    System.out.println(a[a[a[0]]]);
+    System.out.println(array_a[array_a[array_a[0]]]);
   }
 }
