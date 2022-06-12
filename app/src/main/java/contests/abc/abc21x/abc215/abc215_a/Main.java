e18250124674cba41dcaf85cd3096c10fdebb1c1
devgenjin77 <devgenjin77@gmail.com>
2022-06-12 12:04:36 +0900

    Fix Solution ABC215_A

--- app/src/main/java/contests/abc/abc21x/abc215/abc215_a/Main.java
+++ app/src/main/java/contests/abc/abc21x/abc215/abc215_a/Main.java
@@ -4,7 +4,7 @@
  * https://atcoder.jp/contests/abc215/tasks/abc215_a
  *
  * verified:
- * - https://atcoder.jp/contests/abc215/submissions/32428968
+ * - https://atcoder.jp/contests/abc215/submissions/32429395
  *
  */
 
@@ -17,8 +17,8 @@ import java.io.InputStreamReader;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String s = br.readLine();
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final String s = br.readLine();
     br.close();
     System.out.println("Hello,World!".equals(s) ? "AC" : "WA");
   }
