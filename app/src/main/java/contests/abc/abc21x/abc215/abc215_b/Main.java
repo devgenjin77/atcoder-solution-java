06b74c1316364ceab5b7e9ba8347a6441626e132
devgenjin77 <devgenjin77@gmail.com>
2022-06-12 11:51:35 +0900

    Resolve Solution ABC215_B

---
+++ app/src/main/java/contests/abc/abc21x/abc215/abc215_b/Main.java
@@ -0,0 +1,25 @@
+/*
+ * AtCoder Beginner Contest 215
+ * B - log2(N)
+ * https://atcoder.jp/contests/abc215/tasks/abc215_b
+ *
+ * verified:
+ * - https://atcoder.jp/contests/abc215/submissions/32429178
+ *
+ */
+
+package contests.abc.abc21x.abc215.abc215_b;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long n = Long.parseLong(br.readLine());
+    br.close();
+    System.out.println(Long.toBinaryString(n).length() - 1);
+  }
+}
