0741d5e53e580490e26ca1c745d94a09b20b693a
devgenjin77 <devgenjin77@gmail.com>
2022-06-16 21:53:04 +0900

    Fix Library NextScanner

---
+++ app/src/main/java/library/io/NextScanner.java
@@ -0,0 +1,49 @@
+package library.io;
+
+//NextScannerライブラリ
+public class NextScanner implements AutoCloseable {
+
+  private final java.io.BufferedReader br;
+
+  private java.util.StringTokenizer st;
+
+  private static final int BUF_SIZE = 1 << 16;
+
+  private static final char[] c_buf = new char[BUF_SIZE];
+
+  public NextScanner(java.io.InputStream input) {
+    this.br = new java.io.BufferedReader(new java.io.InputStreamReader(input));
+  }
+
+  private java.util.StringTokenizer readInput() {
+    java.util.StringTokenizer st;
+    try {
+      int b = br.read(c_buf);
+      if (b == BUF_SIZE) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(c_buf);
+        sb.append(br.readLine());
+        st = new java.util.StringTokenizer(sb.toString());
+      } else if (b < 0) {
+        throw new java.util.NoSuchElementException();
+      } else {
+        st = new java.util.StringTokenizer(new String(c_buf, 0, b));
+      }
+    } catch (java.io.IOException e) {
+      throw new java.util.InputMismatchException(e.getMessage());
+    }
+    return st;
+  }
+
+  public String next() throws java.io.IOException {
+    if (st == null || !st.hasMoreElements()) {
+      st = readInput();
+    }
+    return st.nextToken();
+  }
+
+  @Override
+  public void close() throws Exception {
+    br.close();
+  }
+}
