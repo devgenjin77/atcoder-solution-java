1cc8a3139141c5183c398f9e9ee5e5b569c78265
devgenjin77 <devgenjin77@gmail.com>
2022-06-23 23:21:18 +0900

    Add Solution ABC218_A

--- app/src/main/java/contests/abc/abc21x/abc218/abc218_a/Main.java
+++ app/src/main/java/contests/abc/abc21x/abc218/abc218_a/Main.java
@@ -1,12 +1,14 @@
 /*
- * ABC218
+ * AtCoder Beginner Contest 218
  * A - Weather Forecast
  * https://atcoder.jp/contests/abc218/tasks/abc218_a
  *
  * verified:
- * - https://atcoder.jp/contests/abc218/submissions/25747332
+ * - https://atcoder.jp/contests/abc218/submissions/32678035
+ *
  */
-package contests.abc.abc218.abc218_a;
+
+package contests.abc.abc21x.abc218.abc218_a;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,9 +17,9 @@ import java.io.InputStreamReader;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    String s = br.readLine();
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final int n = Integer.parseInt(br.readLine());
+    final String s = br.readLine();
     br.close();
     System.out.println(s.charAt(n - 1) == 'o' ? "Yes" : "No");
   }
