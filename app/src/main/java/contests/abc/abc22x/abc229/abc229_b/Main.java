4fbc33a475c8a05be8be6c19829c1a8f1508db24
devgenjin77 <devgenjin77@gmail.com>
2022-08-08 18:24:53 +0900

    Replace Solution ABC229_B

--- app/src/main/java/contests/abc/abc22x/abc229/abc229_b/Main.java
+++ app/src/main/java/contests/abc/abc22x/abc229/abc229_b/Main.java
@@ -1,25 +1,32 @@
 /*
- * ABC229
+ * NECプログラミングコンテスト2021
+ * （AtCoder Beginner Contest 229）
  * B - Hard Calculation
  * https://atcoder.jp/contests/abc229/tasks/abc229_b
  *
  * verified:
- * - https://atcoder.jp/contests/abc229/submissions/27559576
+ * - https://atcoder.jp/contests/abc229/submissions/33879176
+ *
+ * note:
+ *
+ *
  */
-package contests.abc.abc229.abc229_b;
+
+package contests.abc.abc22x.abc229.abc229_b;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] ab = br.readLine().split(" ");
+    final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    final StringTokenizer st = new StringTokenizer(br.readLine());
+    long a = Long.parseLong(st.nextToken());
+    long b = Long.parseLong(st.nextToken());
     br.close();
-    long a = Long.parseLong(ab[0]);
-    long b = Long.parseLong(ab[1]);
     boolean isHard = false;
     while (a > 0 && b > 0) {
       if ((a % 10) + (b % 10) > 9) {
