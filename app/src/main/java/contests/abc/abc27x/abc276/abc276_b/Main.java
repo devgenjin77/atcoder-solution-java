eb1b61e3c65218df8b8fef3dac021cf3ebbcfcaf
devgenjin77 <devgenjin77@gmail.com>
2022-11-06 21:32:27 +0900

    Add Solution ABC276_B

---
+++ app/src/main/java/contests/abc/abc27x/abc276/abc276_b/Main.java
@@ -0,0 +1,167 @@
+/*
+ * AtCoder Beginner Contest 276
+ * B - Adjacency List
+ * https://atcoder.jp/contests/abc276/tasks/abc276_b
+ *
+ * verified:
+ * - https://atcoder.jp/contests/abc276/submissions/36287801
+ *
+ * note:
+ *
+ *
+ */
+
+package contests.abc.abc27x.abc276.abc276_b;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    final FastScanner sc = new FastScanner(System.in);
+    final int n = sc.nextInt();
+    final int m = sc.nextInt();
+    final List<List<Integer>> list_adj = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      list_adj.add(new ArrayList<>());
+    }
+    for (int i = 0; i < m; i++) {
+      int a = sc.nextInt() - 1;
+      int b = sc.nextInt() - 1;
+      list_adj.get(a).add(b);
+      list_adj.get(b).add(a);
+    }
+    sc.close();
+    PrintWriter pw = new PrintWriter(System.out);
+    for (int i = 0; i < n; i++) {
+      List<Integer> l = list_adj.get(i);
+      if (l.size() == 0) {
+        pw.println(0);
+      } else {
+        StringBuilder sb = new StringBuilder();
+        sb.append(l.size()).append(' ');
+        Collections.sort(l);
+        for (int v : l) {
+          sb.append(v + 1).append(' ');
+        }
+        pw.println(sb.deleteCharAt(sb.length() - 1));
+      }
+    }
+    pw.close();
+  }
+
+  // FastScannerライブラリ
+  static class FastScanner implements AutoCloseable {
+
+    private final java.io.InputStream in;
+    private final byte[] buf = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    FastScanner(java.io.InputStream source) {
+      this.in = source;
+    }
+
+    private boolean hasNextByte() {
+      if (ptr < buflen) {
+        return true;
+      } else {
+        ptr = 0;
+        try {
+          buflen = in.read(buf);
+        } catch (java.io.IOException e) {
+          e.printStackTrace();
+        }
+        if (buflen <= 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private int readByte() {
+      if (hasNextByte()) {
+        return buf[ptr++];
+      } else {
+        return -1;
+      }
+    }
+
+    private boolean isPrintableChar(int c) {
+      return 33 <= c && c <= 126;
+    }
+
+    private boolean isNumeric(int c) {
+      return '0' <= c && c <= '9';
+    }
+
+    private void skipToNextPrintableChar() {
+      while (hasNextByte() && !isPrintableChar(buf[ptr])) {
+        ptr++;
+      }
+    }
+
+    public boolean hasNext() {
+      skipToNextPrintableChar();
+      return hasNextByte();
+    }
+
+    public String next() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      StringBuilder ret = new StringBuilder();
+      int b = readByte();
+      while (isPrintableChar(b)) {
+        ret.appendCodePoint(b);
+        b = readByte();
+      }
+      return ret.toString();
+    }
+
+    public long nextLong() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      long ret = 0;
+      int b = readByte();
+      boolean negative = false;
+      if (b == '-') {
+        negative = true;
+        if (hasNextByte()) {
+          b = readByte();
+        }
+      }
+      if (!isNumeric(b)) {
+        throw new NumberFormatException();
+      }
+      while (true) {
+        if (isNumeric(b)) {
+          ret = ret * 10 + b - '0';
+        } else if (b == -1 || !isPrintableChar(b)) {
+          return negative ? -ret : ret;
+        } else {
+          throw new NumberFormatException();
+        }
+        b = readByte();
+      }
+    }
+
+    public int nextInt() {
+      return (int) nextLong();
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    @Override
+    public void close() throws Exception {
+      in.close();
+    }
+  }
+}
+
