95a963770290a6760c3827a6d91def2406bd98e6
devgenjin77 <devgenjin77@gmail.com>
2022-12-27 20:31:16 +0900

    Add Solution ABC283_B

---
+++ app/src/main/java/contests/abc/abc28x/abc283/abc283_b/Main.java
@@ -0,0 +1,153 @@
+/*
+ * ユニークビジョンプログラミングコンテスト2022 冬
+ * （AtCoder Beginner Contest 283）
+ * B - First Query Problem
+ * https://atcoder.jp/contests/abc283/tasks/abc283_b
+ *
+ * verified:
+ * - https://atcoder.jp/contests/abc283/submissions/37582144
+ *
+ * note:
+ *
+ *
+ */
+
+package contests.abc.abc28x.abc283.abc283_b;
+
+import java.io.PrintWriter;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    final FastScanner sc = new FastScanner(System.in);
+    final int n = sc.nextInt();
+    final int[] array_a = new int[n];
+    for (int i = 0; i < n; i++) {
+      array_a[i] = sc.nextInt();
+    }
+    final int q = sc.nextInt();
+    final PrintWriter pw = new PrintWriter(System.out);
+    for (int i = 0; i < q; i++) {
+      int type = sc.nextInt();
+      int k = sc.nextInt() - 1;
+      if (type == 1) {
+        array_a[k] = sc.nextInt();
+      } else if (type == 2) {
+        pw.println(array_a[k]);
+      }
+    }
+    pw.close();
+    sc.close();
+  }
+
+  // FastScannerライブラリ
+  static class FastScanner implements AutoCloseable {
+
+    private final java.io.InputStream in;
+    private final byte[] buf = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    FastScanner(java.io.InputStream source) {
+      this.in = source;
+    }
+
+    private boolean hasNextByte() {
+      if (ptr < buflen) {
+        return true;
+      } else {
+        ptr = 0;
+        try {
+          buflen = in.read(buf);
+        } catch (java.io.IOException e) {
+          e.printStackTrace();
+        }
+        if (buflen <= 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private int readByte() {
+      if (hasNextByte()) {
+        return buf[ptr++];
+      } else {
+        return -1;
+      }
+    }
+
+    private boolean isPrintableChar(int c) {
+      return 33 <= c && c <= 126;
+    }
+
+    private boolean isNumeric(int c) {
+      return '0' <= c && c <= '9';
+    }
+
+    private void skipToNextPrintableChar() {
+      while (hasNextByte() && !isPrintableChar(buf[ptr])) {
+        ptr++;
+      }
+    }
+
+    public boolean hasNext() {
+      skipToNextPrintableChar();
+      return hasNextByte();
+    }
+
+    public String next() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      StringBuilder ret = new StringBuilder();
+      int b = readByte();
+      while (isPrintableChar(b)) {
+        ret.appendCodePoint(b);
+        b = readByte();
+      }
+      return ret.toString();
+    }
+
+    public long nextLong() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      long ret = 0;
+      int b = readByte();
+      boolean negative = false;
+      if (b == '-') {
+        negative = true;
+        if (hasNextByte()) {
+          b = readByte();
+        }
+      }
+      if (!isNumeric(b)) {
+        throw new NumberFormatException();
+      }
+      while (true) {
+        if (isNumeric(b)) {
+          ret = ret * 10 + b - '0';
+        } else if (b == -1 || !isPrintableChar(b)) {
+          return negative ? -ret : ret;
+        } else {
+          throw new NumberFormatException();
+        }
+        b = readByte();
+      }
+    }
+
+    public int nextInt() {
+      return (int) nextLong();
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    @Override
+    public void close() throws Exception {
+      in.close();
+    }
+  }
+}
