43dc60b24f9d9c788aa6f10d6640e277429ad2e5
devgenjin77 <devgenjin77@gmail.com>
2023-01-12 01:26:29 +0900

    Add Solution ABC204_C

---
+++ app/src/main/java/contests/abc/abc20x/abc204/abc204_c/Main.java
@@ -0,0 +1,170 @@
+/*
+ * AtCoder Beginner Contest 204
+ * C - Tour
+ * https://atcoder.jp/contests/abc204/tasks/abc204_c
+ *
+ * verified:
+ * - https://atcoder.jp/contests/abc204/submissions/37947839
+ *
+ * note:
+ *
+ *
+ */
+
+package contests.abc.abc20x.abc204.abc204_c;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    final FastScanner sc = new FastScanner(System.in);
+    final int n = sc.nextInt();
+    final int m = sc.nextInt();
+    final List<List<Integer>> list_adj = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      list_adj.add(new ArrayList<>());
+    }
+    for (int i = 0; i < m; i++) {
+      int a = sc.nextInt() - 1;
+      int b = sc.nextInt() - 1;
+      list_adj.get(a).add(b);
+    }
+    sc.close();
+    final Queue<Integer> queue = new ArrayDeque<>();
+    final boolean[] visited = new boolean[n];
+    int ans = 0;
+    //各始点からBFS
+    for (int i = 0; i < n; i++) {
+      queue.add(i);
+      Arrays.fill(visited, false);
+      visited[i] = true;
+      while (!queue.isEmpty()) {
+        int v = queue.poll();
+        ans++;
+        for (int next : list_adj.get(v)) {
+          if (!visited[next]) {
+            visited[next] = true;
+            queue.add(next);
+          }
+        }
+      }
+    }
+    System.out.println(ans);
+  }
+
+  // FastScannerライブラリ
+  static class FastScanner implements AutoCloseable {
+
+    private final java.io.InputStream in;
+    private final byte[] buf = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    FastScanner(java.io.InputStream source) {
+      this.in = source;
+    }
+
+    private boolean hasNextByte() {
+      if (ptr < buflen) {
+        return true;
+      } else {
+        ptr = 0;
+        try {
+          buflen = in.read(buf);
+        } catch (java.io.IOException e) {
+          e.printStackTrace();
+        }
+        if (buflen <= 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private int readByte() {
+      if (hasNextByte()) {
+        return buf[ptr++];
+      } else {
+        return -1;
+      }
+    }
+
+    private boolean isPrintableChar(int c) {
+      return 33 <= c && c <= 126;
+    }
+
+    private boolean isNumeric(int c) {
+      return '0' <= c && c <= '9';
+    }
+
+    private void skipToNextPrintableChar() {
+      while (hasNextByte() && !isPrintableChar(buf[ptr])) {
+        ptr++;
+      }
+    }
+
+    public boolean hasNext() {
+      skipToNextPrintableChar();
+      return hasNextByte();
+    }
+
+    public String next() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      StringBuilder ret = new StringBuilder();
+      int b = readByte();
+      while (isPrintableChar(b)) {
+        ret.appendCodePoint(b);
+        b = readByte();
+      }
+      return ret.toString();
+    }
+
+    public long nextLong() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      long ret = 0;
+      int b = readByte();
+      boolean negative = false;
+      if (b == '-') {
+        negative = true;
+        if (hasNextByte()) {
+          b = readByte();
+        }
+      }
+      if (!isNumeric(b)) {
+        throw new NumberFormatException();
+      }
+      while (true) {
+        if (isNumeric(b)) {
+          ret = ret * 10 + b - '0';
+        } else if (b == -1 || !isPrintableChar(b)) {
+          return negative ? -ret : ret;
+        } else {
+          throw new NumberFormatException();
+        }
+        b = readByte();
+      }
+    }
+
+    public int nextInt() {
+      return (int) nextLong();
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    @Override
+    public void close() throws Exception {
+      in.close();
+    }
+  }
+}
