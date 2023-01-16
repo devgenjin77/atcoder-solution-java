3c0d5c2d52e4a2f0a9ae661828d06bd8ad784bce
devgenjin77 <devgenjin77@gmail.com>
2023-01-16 20:57:18 +0900

    Add Solution ABC285_D

---
+++ app/src/main/java/contests/abc/abc28x/abc285/abc285_d/Main.java
@@ -0,0 +1,233 @@
+/*
+ * AtCoder Beginner Contest 285
+ * D - Change Usernames
+ * https://atcoder.jp/contests/abc285/tasks/abc285_d
+ *
+ * verified:
+ * - https://atcoder.jp/contests/abc285/submissions/38099861
+ *
+ * note:
+ *
+ *
+ */
+
+package contests.abc.abc28x.abc285.abc285_d;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    final FastScanner sc = new FastScanner(System.in);
+    final int n = sc.nextInt();
+    final String[] array_s = new String[n];
+    final String[] array_t = new String[n];
+    final Map<String, Integer> id_map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      array_s[i] = sc.next();
+      array_t[i] = sc.next();
+      if (!id_map.containsKey(array_s[i])) {
+        id_map.put(array_s[i], id_map.size());
+      }
+      if (!id_map.containsKey(array_t[i])) {
+        id_map.put(array_t[i], id_map.size());
+      }
+    }
+    sc.close();
+    final DisjointSetUnion dsu = new DisjointSetUnion(id_map.size());
+    boolean isOK = true;
+    for (int i = 0; i < n; i++) {
+      int id1 = id_map.get(array_s[i]);
+      int id2 = id_map.get(array_t[i]);
+      if (dsu.isSame(id1, id2)) {
+        isOK = false;
+        break;
+      } else {
+        dsu.merge(id1, id2);
+      }
+    }
+    System.out.println(isOK ? "Yes" : "No");
+  }
+
+  //DisjointSetUnionライブラリ
+  static class DisjointSetUnion {
+
+    private final int n;
+    private final int[] parent_or_size;
+
+    public DisjointSetUnion(int n) {
+      this.n = n;
+      parent_or_size = new int[n];
+      java.util.Arrays.fill(parent_or_size, -1);
+    }
+
+    public int merge(int a, int b) {
+      if (!(0 <= a && a < n) || !(0 <= b && b < n)) {
+        return -1;
+      }
+      int x = leader(a);
+      int y = leader(b);
+      if (x == y) {
+        return x;
+      }
+      if (-parent_or_size[x] < -parent_or_size[y]) {
+        int temp = x;
+        x = y;
+        y = temp;
+      }
+      parent_or_size[x] += parent_or_size[y];
+      parent_or_size[y] = x;
+      return x;
+    }
+
+    public boolean isSame(int a, int b) {
+      if (!(0 <= a && a < n) || !(0 <= b && b < n)) {
+        return false;
+      }
+      return leader(a) == leader(b);
+    }
+
+    public int leader(int a) {
+      if (parent_or_size[a] < 0) {
+        return a;
+      } else {
+        parent_or_size[a] = leader(parent_or_size[a]);
+        return parent_or_size[a];
+      }
+    }
+
+    public int size(int a) {
+      if (!(0 <= a && a < n)) {
+        return -1;
+      }
+      return -parent_or_size[leader(a)];
+    }
+
+    public java.util.List<java.util.List<Integer>> groups() {
+      int m = 0;
+      java.util.List<java.util.List<Integer>> result = new java.util.ArrayList<>();
+      java.util.Map<Integer, Integer> map_leader = new java.util.HashMap<>();
+      for (int i = 0; i < n; i++) {
+        int lead = leader(i);
+        if (!map_leader.containsKey(lead)) {
+          map_leader.put(lead, m++);
+          result.add(new java.util.ArrayList<>());
+        }
+        result.get(map_leader.get(lead)).add(i);
+      }
+      return result;
+    }
+  }
+
+  // FastScannerライブラリ
+  static class FastScanner implements AutoCloseable {
+
+    private final java.io.InputStream in;
+    private final byte[] buf = new byte[1024];
+    private int ptr = 0;
+    private int buflen = 0;
+
+    FastScanner(java.io.InputStream source) {
+      this.in = source;
+    }
+
+    private boolean hasNextByte() {
+      if (ptr < buflen) {
+        return true;
+      } else {
+        ptr = 0;
+        try {
+          buflen = in.read(buf);
+        } catch (java.io.IOException e) {
+          e.printStackTrace();
+        }
+        if (buflen <= 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private int readByte() {
+      if (hasNextByte()) {
+        return buf[ptr++];
+      } else {
+        return -1;
+      }
+    }
+
+    private boolean isPrintableChar(int c) {
+      return 33 <= c && c <= 126;
+    }
+
+    private boolean isNumeric(int c) {
+      return '0' <= c && c <= '9';
+    }
+
+    private void skipToNextPrintableChar() {
+      while (hasNextByte() && !isPrintableChar(buf[ptr])) {
+        ptr++;
+      }
+    }
+
+    public boolean hasNext() {
+      skipToNextPrintableChar();
+      return hasNextByte();
+    }
+
+    public String next() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      StringBuilder ret = new StringBuilder();
+      int b = readByte();
+      while (isPrintableChar(b)) {
+        ret.appendCodePoint(b);
+        b = readByte();
+      }
+      return ret.toString();
+    }
+
+    public long nextLong() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      long ret = 0;
+      int b = readByte();
+      boolean negative = false;
+      if (b == '-') {
+        negative = true;
+        if (hasNextByte()) {
+          b = readByte();
+        }
+      }
+      if (!isNumeric(b)) {
+        throw new NumberFormatException();
+      }
+      while (true) {
+        if (isNumeric(b)) {
+          ret = ret * 10 + b - '0';
+        } else if (b == -1 || !isPrintableChar(b)) {
+          return negative ? -ret : ret;
+        } else {
+          throw new NumberFormatException();
+        }
+        b = readByte();
+      }
+    }
+
+    public int nextInt() {
+      return (int) nextLong();
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    @Override
+    public void close() throws Exception {
+      in.close();
+    }
+  }
+}
